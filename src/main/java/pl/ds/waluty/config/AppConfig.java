0d1f3359c56e25d52cb6174333f1393d69108129
jaczaw <zawislak.jacek@gmail.com>
2022-01-05 14:20:24 +0100

    Pozostala część - zabrakło czasu.

--- src/main/java/pl/ds/waluty/config/AppConfig.java
+++ src/main/java/pl/ds/waluty/config/AppConfig.java
@@ -1,5 +1,9 @@
 package pl.ds.waluty.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -10,5 +14,15 @@ public class AppConfig {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI(@Value("${app.description}") String appDescription,
+                                 @Value("${app.version}") String appVersion) {
+
+        return new OpenAPI().info(new Info().title("Prototym aplikacji Zakup-Walut w NBP API").version(appVersion)
+                .description(appDescription).termsOfService("http://swagger.io/terms/")
+                .license(new License().name("Apache 2.0").url("http://springdoc.org")));
+
     }
 }
