458ca432aa0be5700d0dafb63fcfb93803fb1037
jaczaw <zawislak.jacek@gmail.com>
2022-01-05 10:29:55 +0100

    Initial commit

---
+++ src/main/java/pl/ds/waluty/WalutyZakupApplication.java
@@ -0,0 +1,13 @@
+package pl.ds.waluty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalutyZakupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalutyZakupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/ds/waluty/WalutyZakupApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.ds.waluty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalutyZakupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
